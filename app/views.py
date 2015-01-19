a80030a807b51ae3cc54a71fe0b44af44f3cbac2
Samuellynch <samuellynch6@gmail.com>
2015-01-19 20:50:30 +0000

    nothing

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = {'nickname': 'Samuel'} '#fakeuser'
+    return '''
+<html>
+    <head>
+        <title>Home page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['nickname'] + '''</h1>
+</html>
+'''
 
