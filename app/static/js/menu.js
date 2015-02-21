6c4ae632c2d000b7f3b18862d0152fae585bb86f
Samuellynch <samuellynch6@gmail.com>
2015-02-21 15:27:03 +0000

    nothing

---
+++ app/static/js/menu.js
@@ -0,0 +1,25 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+    $('.slideout-menu-toggle').on('click', function(event){
+    	event.preventDefault();
+    	// create menu variables
+    	var slideoutMenu = $('.slideout-menu');
+    	var slideoutMenuWidth = $('.slideout-menu').width();
+
+    	// toggle open class
+    	slideoutMenu.toggleClass("open");
+
+    	// slide menu
+    	if (slideoutMenu.hasClass("open")) {
+	    	slideoutMenu.animate({
+		    	left: "0px"
+	    	});
+    	} else {
+	    	slideoutMenu.animate({
+		    	left: -slideoutMenuWidth
+	    	}, 250);
+    	}
+    });
+});
+</script>
--- app/views.py
+++ app/views.py
@@ -4,18 +4,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'samuel'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
                            title='Home',
-                           user=user,
-                           posts=posts)
+                           user=user)
