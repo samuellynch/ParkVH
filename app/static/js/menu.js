3011d6835b5b17d6ce1016b6ce7e27d62410c5b2
Samuellynch <samuellynch6@gmail.com>
2015-02-21 19:10:06 +0000

    nothing

--- app/static/js/menu.js
+++ app/static/js/menu.js
@@ -1,25 +1,39 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function () {
-    $('.slideout-menu-toggle').on('click', function(event){
-    	event.preventDefault();
-    	// create menu variables
-    	var slideoutMenu = $('.slideout-menu');
-    	var slideoutMenuWidth = $('.slideout-menu').width();
-    	
-    	// toggle open class
-    	slideoutMenu.toggleClass("open");
-    	
-    	// slide menu
-    	if (slideoutMenu.hasClass("open")) {
-	    	slideoutMenu.animate({
-		    	left: "0px"
-	    	});	
-    	} else {
-	    	slideoutMenu.animate({
-		    	left: -slideoutMenuWidth
-	    	}, 250);	
-    	}
-    });
-});
-</script>
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200) ;
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
+
+
+
+
+
+
+(function() {
+                var body = $('body');
+                $('.menu-toggle').bind('click', function(){
+                    body.toggleClass('menu-open');
+                    return false;
+                });
+            })();
