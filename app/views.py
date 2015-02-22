f53628af07fe73211519df99a7cff98c0e850f95
Samuellynch <samuellynch6@gmail.com>
2015-02-22 15:09:48 +0000

    nothing

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
+    user = {'nickname': 'samuel'}  # fake user
     return render_template('index.html',
                            title='Home',
+                           user=user)
+
+@app.route("/trainingprogramme")
+def trainingprogramme():
+    user = {'nickname': 'samuel'}
+    return render_template("trainingprogramme.html",
+                           title='trainingprogramme',
                            user=user)
