2e8bde3b8760f29ec6524b2eb91bf5941b13306d
Samuellynch <samuellynch6@gmail.com>
2015-02-22 15:41:52 +0000

    nothing

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template
 from app import app
 
 @app.route('/')
@@ -14,4 +14,25 @@ def trainingprogramme():
     user = {'nickname': 'samuel'}
     return render_template("trainingprogramme.html",
                            title='trainingprogramme',
+                           user=user)
+
+@app.route("/trainingprogress")
+def trainingprogress():
+    user = {'nickname': 'samuel'}
+    return render_template("trainingprogress.html",
+                           title='trainingprogress',
+                           user=user)
+
+@app.route("/personalbest")
+def personalbest():
+    user = {'nickname': 'samuel'}
+    return render_template("personalbest.html",
+                           title='personalbest',
+                           user=user)
+
+@app.route("/profile")
+def profile():
+    user = {'nickname': 'samuel'}
+    return render_template("profile.html",
+                           title='profile',
                            user=user)
