49983b38b5af8c26378b69bb6f108d0da27539f9
samuellynch <samuellynch6@gmail.com>
2015-02-27 11:06:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from distuticls.core import setup
 
 setup(name='ParkVH',
       version='1.0',
+      description='Parkwood vale harriers',
+      author='Samuel Lynch',
+      packages=['Flask']
+      )
       
