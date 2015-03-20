2aa6cc75118ed0ce00ec6caab46c4ecab83e4176
samuellynch <samuellynch6@gmail.com>
2015-03-20 12:17:20 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -6,7 +6,8 @@ from app import app
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin''user' or request.form['password'] != 'admin''password':
+            
             error = 'Invalid Credentials. Please try again.'
         else:
 
